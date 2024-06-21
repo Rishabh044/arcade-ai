facee960c16ce1ae937773d755e4474451d4751c
Sam Partee <Partees21@gmail.com>
2024-06-21 14:29:58 -0700

    Migrate to arcade py package

---
+++ arcade/arcade/actor/common/serializers.py
@@ -0,0 +1,13 @@
+import msgspec
+from typing import Any, Sequence
+
+from starlette.responses import JSONResponse
+
+
+class MsgSpecJSONResponse(JSONResponse):
+    """
+    JSON response using the high-performance msgspec library to serialize data to JSON.
+    """
+
+    def render(self, content: Any) -> bytes:
+        return msgspec.json.encode(content)
