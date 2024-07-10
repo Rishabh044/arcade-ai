a12f63e57077f1d2a8eae0d414692d9dd5b08674
Nate Barbettini <nate@arcade-ai.com>
2024-07-10 14:11:45 -0700

    More cleanup

--- arcade/tests/sdk/test_tool_decorator.py
+++ arcade/tests/sdk/test_tool_decorator.py
@@ -1,12 +1,16 @@
-import pytest
 import asyncio
+
+import pytest
+
 from arcade.sdk.tool import tool
+
 
 @pytest.mark.asyncio
 async def test_sync_function():
     """
     Ensures a sync function will run when decorated by @tool
     """
+
     @tool
     def sync_func(x, y):
         return x + y
@@ -14,14 +18,16 @@ async def test_sync_function():
     result = await sync_func(1, 2)
     assert result == 3
 
+
 @pytest.mark.asyncio
 async def test_async_function():
     """
     Ensures an async function will run when decorated by @tool
     """
+
     @tool
     async def async_func(x, y):
-        await asyncio.sleep(0);
+        await asyncio.sleep(0)
         return x + y
 
     result = await async_func(1, 2)
