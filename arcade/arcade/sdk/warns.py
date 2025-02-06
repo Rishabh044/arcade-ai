227e84b1a033205f721a811055244e94cb816f41
Renato Byrro <rmbyrro@gmail.com>
2025-02-06 17:01:55 -0300

    add type annotations for decorator interfaces

--- arcade/arcade/sdk/warns.py
+++ arcade/arcade/sdk/warns.py
@@ -1,11 +1,12 @@
 import warnings
 from functools import wraps
+from typing import Any, Callable
 
 
-def deprecated(reason: str, stacklevel: int = 3):
-    def decorator(func):
+def deprecated(reason: str, stacklevel: int = 3) -> Callable:
+    def decorator(func: Callable) -> Callable:
         @wraps(func)
-        async def wrapper(*args, **kwargs):
+        async def wrapper(*args: Any, **kwargs: Any) -> Any:
             warnings.warn(
                 f"{func.__name__} is deprecated: {reason}",
                 DeprecationWarning,
