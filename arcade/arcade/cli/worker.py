9476d830243c36e1e31a3c7ef5df75fbfb648dbb
sdreyer <sterling@arcade-ai.com>
2025-03-11 14:31:08 -0700

    More logging

--- arcade/arcade/cli/worker.py
+++ arcade/arcade/cli/worker.py
@@ -66,15 +66,53 @@ def main(
 
 
 @app.command("list", help="List all workers")
-def list_workers() -> None:
+def list_workers(
+    cloud_host: str = typer.Option(
+        PROD_CLOUD_HOST,
+        "--cloud-host",
+        "-c",
+        help="The Arcade Engine host.",
+        hidden=True,
+    ),
+    cloud_port: int = typer.Option(
+        None,
+        "--cloud-port",
+        "-cp",
+        help="The port of the Arcade Engine host.",
+        hidden=True,
+    ),
+    force_tls: bool = typer.Option(
+        False,
+        "--tls",
+        help="Whether to force TLS for the connection to the Arcade Engine.",
+        hidden=True,
+    ),
+    force_no_tls: bool = typer.Option(
+        False,
+        "--no-tls",
+        help="Whether to disable TLS for the connection to the Arcade Engine.",
+        hidden=True,
+    ),
+) -> None:
     config = validate_and_get_config()
     engine_url = state["engine_url"]
     client = Arcade(api_key=config.api.key, base_url=engine_url)
+    deployments = []
+    try:
+        cloud_url = compute_base_url(force_tls, force_no_tls, cloud_host, cloud_port)
+        cloud_client = httpx.Client(base_url=cloud_url)
+        response = cloud_client.get(
+            "/api/v1/workers", headers={"Authorization": f"Bearer {config.api.key}"}
+        )
+        response.raise_for_status()
+        deployments = response.json()["data"]["workers"]
+    except Exception as e:
+        console.log(f"Failed to get cloud deployments: {e}")
 
-    print_worker_table(client)
+    print_worker_table(client, deployments)
 
 
-def print_worker_table(client: Arcade) -> None:
+def print_worker_table(client: Arcade, deployments: list[dict]) -> None:
     workers = client.workers.list()
     if not workers.items:
         console.print("No workers found", style="bold red")
@@ -83,18 +121,53 @@ def print_worker_table(client: Arcade) -> None:
     # Create and print a table of worker information
     table = Table(title="Workers")
     table.add_column("ID")
+    table.add_column("Cloud Deployed")
+    table.add_column("Engine Registered")
     table.add_column("Enabled")
     table.add_column("Host")
     table.add_column("Toolkits")
+
+    # Track workers that are registered in the engine
+    engine_workers = []
     for worker in workers.items:
+        if worker.id is None:
+            continue
+        engine_workers.append(worker.id)
+
+        # Check if the worker is deployed in the cloud
+        is_deployed = is_cloud_deployment(worker.id, deployments)
+        # Get the toolkits for the worker
+
         tools = get_toolkits(client, worker.id)
+        uri = worker.http.uri if worker.http and worker.http.uri else ""
         table.add_row(
             worker.id,
+            str(is_deployed),
+            str(True),
             str(worker.enabled),
-            worker.http.uri if worker.http else "",
-            "Cannot get toolkits from disabled workers" if tools == "" else tools,
+            compare_endpoints(worker.id, uri, deployments),
+            "Could not fetch toolkits" if tools == "" else tools,
         )
+    for deployment in deployments:
+        if deployment["name"] not in engine_workers:
+            table.add_row(deployment["name"], "True", "False", "False", deployment["endpoint"], "")
     console.print(table)
+
+
+# Check if the worker is in the list of cloud deployments
+def is_cloud_deployment(name: str, deployments: list[dict]) -> bool:
+    return any(deployment["name"] == name for deployment in deployments)
+
+
+# Compare the endpoint of the worker in the engine to the endpoint in the cloud
+# Return a highlighted diff if the endpoint in the engine is different from the endpoint in the cloud
+def compare_endpoints(worker_id: str, engine_endpoint: str, deployments: list[dict]) -> str:
+    if is_cloud_deployment(worker_id, deployments):
+        for deployment in deployments:
+            if deployment["endpoint"] == engine_endpoint:
+                return engine_endpoint
+            return f"[red]Endpoint Mismatch\nRegistered Endpoint: {engine_endpoint}\nActual Endpoint {deployment["endpoint"]}[/red]"
+    return engine_endpoint
 
 
 def parse_deployment_response(response: dict) -> None:
@@ -226,6 +299,59 @@ def rm_worker(
         console.print("Worker not found", style="bold red")
     except Exception as e:
         console.print(f"Error deleting worker from engine: {e}", style="bold red")
+        raise typer.Exit(code=1)
+
+
+@app.command("logs", help="Get logs for a worker")
+def worker_logs(
+    worker_id: str,
+    cloud_host: str = typer.Option(
+        PROD_CLOUD_HOST,
+        "--cloud-host",
+        "-c",
+        help="The Arcade Engine host.",
+        hidden=True,
+    ),
+    cloud_port: int = typer.Option(
+        None,
+        "--cloud-port",
+        "-cp",
+        help="The port of the Arcade Engine host.",
+        hidden=True,
+    ),
+    force_tls: bool = typer.Option(
+        False,
+        "--tls",
+        help="Whether to force TLS for the connection to the Arcade Engine.",
+        hidden=True,
+    ),
+    force_no_tls: bool = typer.Option(
+        False,
+        "--no-tls",
+        help="Whether to disable TLS for the connection to the Arcade Engine.",
+        hidden=True,
+    ),
+) -> None:
+    config = validate_and_get_config()
+    cloud_url = compute_base_url(force_tls, force_no_tls, cloud_host, cloud_port)
+
+    try:
+        with httpx.stream(
+            "GET",
+            f"{cloud_url}/api/v1/workers/logs/{worker_id}",
+            headers={"Authorization": f"Bearer {config.api.key}"},
+            # allow the connection to stay open indefinitely
+            timeout=None,  # noqa: S113
+        ) as s:
+            for line in s.iter_lines():
+                if not line:  # Skip empty lines
+                    continue
+                if line.startswith("data:"):
+                    # Extract just the data portion after 'data:'
+                    data = line[5:].strip()  # Remove 'data:' prefix and whitespace
+                    console.print(data)
+    except Exception as e:
+        console.print(f"Error connecting to log stream: {e}", style="bold red")
         raise typer.Exit(code=1)
 
 
